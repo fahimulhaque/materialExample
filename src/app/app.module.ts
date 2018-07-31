f8a9c84081beaaeb372a96456c534ce7af50a18f
fahimulhaque <ffahimulhaque9@gamil.com>
2018-08-01 00:10:26 +0530

    Angualr Tool Tip Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule , MatInputModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MyFormComponent } from './my-form/my-form.component';
+import { MatDialogModule , MatCardModule , MatTooltipModule } from '@angular/material';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+import { TooltipDemoComponent } from './tooltip-demo/tooltip-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent,
+    DialogDemoComponent,
+    MyDialogComponent,
+    TooltipDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
+    MatCardModule,
+    MatTooltipModule
   ],
+  entryComponents: [MyDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Address } from '../address';
+
+@Component({
+  selector: 'app-my-form',
+  templateUrl: './my-form.component.html',
+  styleUrls: ['./my-form.component.css']
+})
+export class MyFormComponent implements OnInit {
+
+  address = new Address();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    alert('Thanks for submiting' + JSON.stringify(this.address) );
+  }
+
+}
